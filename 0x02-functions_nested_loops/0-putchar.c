238258c59fb8255ece478bb121911af8509b61d9
victorchidozie47 <victorchidozie47@gmail.com>
2022-07-09 04:07:26 -0700

    0 task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
