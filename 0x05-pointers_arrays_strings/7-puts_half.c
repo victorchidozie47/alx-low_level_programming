f712161e0c4606505408d5542bdda62d17f31ade
victorchidozie47 <victorchidozie47@gmail.com>
2022-07-12 13:54:28 -0700

    7 task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half os a string
+ * @str: String considered
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	if (a % 2 == 1)
+	{
+		b = (a - 1) / 2;
+		b += 1;
+	}
+	else
+	{
+		b = a / 2;
+	}
+
+	for (; b < a; b++)
+	{
+		_putchar(str[b]);
+	}
+
+	_putchar('\n');
+}
