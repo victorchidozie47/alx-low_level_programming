6115f7dc6bb65e5abfa97c4b079a172aa9b6cbf6
victorchidozie47 <victorchidozie47@gmail.com>
2022-07-14 15:01:21 -0700

    9 task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer with only putchar
+ * @n: Number to print
+ *
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
