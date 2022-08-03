3abcf833a3d5150ba973cb09d31dc93e6a7644aa
victorchidozie47 <victorchidozie47@gmail.com>
2022-08-03 14:14:01 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+	* print_name - Prints a name
+	*
+	* @name: Name to be printed
+	* @f: Pointer casted to the char pointer
+	* Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	f(name);
+}
