6eefadf8759e5129988db5118c50eb5be50aeeb3
victorchidozie47 <victorchidozie47@gmail.com>
2022-08-16 15:52:12 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -86,4 +86,14 @@ size_t free_listint_safe(listint_t **h)
 		for (index = 0; index < nodes; index++)
 		{
 			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
+}
