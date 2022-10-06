32711386b5c70e3445c6bb94884817ff6853598e
victorchidozie47 <victorchidozie47@gmail.com>
2022-10-06 00:17:47 -0700

    0x18

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+
+	dest[len] = '\0';
+	return (dest);
+}
